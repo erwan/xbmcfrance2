dba97f4154f654210ef7fb0f30575e9d7994e36c
Loisant Senda <eloisant@gmail.com>
2008-10-12 12:51:14 -0700

    prefetch, bigger font

--- default.py
+++ default.py
@@ -49,6 +49,7 @@ class France2GUI(xbmcgui.Window):
       main_list.addItem(xbmcgui.ListItem(label="Journal de 13H"))
       main_list.addItem(xbmcgui.ListItem(label="Journal de 20H"))
       self.setFocus(main_list)
+      prefetch_info()
 
     except:
       xbmc.log('Exception (init): ' + str(sys.exc_info()[0]))
@@ -81,13 +82,17 @@ class France2GUI(xbmcgui.Window):
     jt = self.f2.get_lastjt(jt)
     self.player.play(jt["url"])
 
+  def prefetch_info(self):
+    for jt in self.f2.PROGRAMS:
+      self.f2.get_lastjt(jt[0])
+
   def refresh_label(self):
     main_list = self.get_control('Program List')
     pos = main_list.getSelectedPosition()
     program = self.f2.PROGRAMS[pos]
     info = self.f2.get_lastjt(program[0])
     lbl = self.get_control('edition name')
-    lbl.setLabel(info["name"])
+    lbl.setLabel(program[0] + " : " + info["name"])
 
   def onAction(self, action):
     """Handle user input events."""
